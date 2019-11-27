e48bd5944a65174714a0cac1fc0c9a7b46116b70
onestep <praneeth.madura@gmail.com>
2019-11-27 13:11:37 +0530

    changes 27.11.2019

--- arava/custom_addons/others/arava_templates/models/inherit_vendor_bills.py
+++ arava/custom_addons/others/arava_templates/models/inherit_vendor_bills.py
@@ -1,18 +1,46 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019-praneeth
+# Copyright (C) 2019-
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_compare
 
 
 class VendorBillsInherit(models.Model):
     _inherit = 'account.invoice.line'
-    gross = fields.Char(string='Gross', compute='_product_weight', readonly=True)
-
-    @api.onchange('product_id')
-    @api.depends('product_id')
-    def _product_weight(self):
-        for record in self:
-            record.gross = record.product_id.name
+    gross = fields.Char(string='Gross Weight', store=True)
+    deduction = fields.Char(string='Deduction %', store=True)
 
 
-VendorBillsInherit()
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    def _prepare_invoice_line_from_po_line(self, line):
+        if line.product_id.purchase_method == 'purchase':
+            qty = line.product_qty - line.qty_invoiced
+        else:
+            qty = line.qty_received - line.qty_invoiced
+        if float_compare(qty, 0.0, precision_rounding=line.product_uom.rounding) <= 0:
+            qty = 0.0
+        taxes = line.taxes_id
+        invoice_line_tax_ids = line.order_id.fiscal_position_id.map_tax(taxes)
+        invoice_line = self.env['account.invoice.line']
+        data = {
+            'purchase_line_id': line.id,
+            'name': line.order_id.name+': '+line.name,
+            'origin': line.order_id.origin,
+            'uom_id': line.product_uom.id,
+            'product_id': line.product_id.id,
+            'account_id': invoice_line.with_context({'journal_id': self.journal_id.id, 'type': 'in_invoice'})._default_account(),
+            'price_unit': line.order_id.currency_id.with_context(date=self.date_invoice).compute(line.price_unit, self.currency_id, round=False),
+            'quantity': qty,
+            'discount': 0.0,
+            'gross': line.gross_weight,
+            'deduction': line.deduction,
+            'account_analytic_id': line.account_analytic_id.id,
+            'analytic_tag_ids': line.analytic_tag_ids.ids,
+            'invoice_line_tax_ids': invoice_line_tax_ids.ids
+        }
+        account = invoice_line.get_invoice_line_account('in_invoice', line.product_id, line.order_id.fiscal_position_id, self.env.user.company_id)
+        if account:
+            data['account_id'] = account.id
+        return data
